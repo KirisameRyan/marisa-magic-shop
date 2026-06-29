aeb05b1042d985f5f6d072373aa02c64d7ca2fb1
hachimi <3895418161@qq.com>
2026-06-29 12:09:41 +0800

    原神启动

--- nav.js
+++ nav.js
@@ -18,6 +18,7 @@
       {
         label: '🆕 最新上线',
         links: [
+          { href: 'quiz-genshin.html',      emoji: '✨', text: '原神人格' },
           { href: 'quiz-major.html',       emoji: '🏗️', text: '张雪峰选专业' },
           { href: 'quiz-otti.html',        emoji: '🎭', text: 'OTTI人格' },
           { href: 'quiz-otokonoko.html',   emoji: '💅', text: '男娘指数' },
